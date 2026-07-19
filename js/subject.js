1f11408b619f5447972037bf8a237f1d17fb274a
StudyWithPrakash <baduprakash13@gmail.com>
2026-07-19 13:26:42 +0545

    Create subject.js

---
+++ js/subject.js
@@ -0,0 +1,47 @@
+const params = new URLSearchParams(window.location.search);
+
+const classId = params.get("class");
+const subject = params.get("subject");
+
+document.getElementById("subject-title").textContent =
+`Class ${classId} - ${subject}`;
+
+async function loadCategories(){
+
+    const response = await fetch("../data/categories.json");
+
+    const data = await response.json();
+
+    const item = data.find(x => x.subject === subject);
+
+    const grid = document.getElementById("category-grid");
+
+    if(!item){
+        grid.innerHTML="<p>No categories found.</p>";
+        return;
+    }
+
+    item.categories.forEach(category=>{
+
+        const card=document.createElement("div");
+
+        card.className="class-card";
+
+        card.textContent=category;
+
+        card.onclick=()=>{
+
+            alert(
+                "Next we will build: "
+                + category
+            );
+
+        };
+
+        grid.appendChild(card);
+
+    });
+
+}
+
+loadCategories();

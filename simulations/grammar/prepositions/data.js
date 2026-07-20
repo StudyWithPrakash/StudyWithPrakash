1be89967d1689bc84b2e6cb9909a6423797902b3
StudyWithPrakash <baduprakash13@gmail.com>
2026-07-20 14:24:09 +0545

    Update data.js

--- simulations/grammar/prepositions/data.js
+++ simulations/grammar/prepositions/data.js
@@ -1,1 +1,28 @@
+const PREPOSITIONS = {
 
+place:[
+"in",
+"on",
+"under",
+"behind",
+"between",
+"next to",
+"in front of"
+],
+
+time:[
+"at",
+"on",
+"in"
+],
+
+movement:[
+"to",
+"into",
+"onto",
+"from",
+"through",
+"across"
+]
+
+};
